994cd48e5afdaccefe658c40aa49106aadc9cafb
Tom Öst <tom.ost@yahoo.com>
2019-08-28 20:33:05 +0200

    Added och changed functions
    Changed functions:
    
    * getData = After success, grab the Pokemon IMG Url and store it. THen push it into originalPokemonImgUrl array.
    
    Added functions:
    
    * duplicate = After getData has executed (setTimeout). Copy originalPokemonImgUrl array into duplicateAllPokemonImgUrl then concat both into allPokemonImgUrl.
    This creates an array that always has two URL to the same images.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,17 @@ let easy = 4;
 let medium = 6;
 let hard = 8;
 
+// Arrays for PokemonImgUrl.
 
-// Function randomNumbers.
+let originalPokemonImgUrl = [];
+let duplicateAllPokemonImgUrl = [];
+let allPokemonImgUrl = [];
+
+
+// PokéAPI URL.
+const pokemonDataUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+// This function creates a random number depending on the settings below. 
 function randomNumber() {
     
     // Settings for max randomnumbers starting from index 1.
@@ -15,38 +24,63 @@ function randomNumber() {
     return Math.floor(Math.random() * randomNumberMax) + fromIndex;
 }
 
-
 // Function for getting data from PokéAPI.
 function getData() {
-    $.ajax ({
+    $.ajax ({ 
         type: 'GET',
-        url: 'https://pokeapi.co/api/v2/pokemon/' + randomNumber(), // Calling randomnnumber to get random pokémon.
+        url: pokemonDataUrl + randomNumber(), // Calling randomnnumber to get random pokémon.
         success: function(pokemonData) {
-            console.log(pokemonData);
-        }
 
+                var pokemonImgUrl = pokemonData.sprites.front_default; // Store and extract pokemon images.
+
+                originalPokemonImgUrl.push(pokemonImgUrl); // store ImagesURL to a global array called allPokemonImgUrl.
+        }
+        
     })
 }
+
+// This function copies the array and concat into a new array so that we always get two pictures each from the API. It executes from a setTimeout.
+function duplicate(){
+
+    setTimeout( function(){
+        duplicateAllPokemonImgUrl = originalPokemonImgUrl.slice();
+        }, 1000 );
+    
+    setTimeout( function(){
+        allPokemonImgUrl = originalPokemonImgUrl.concat(duplicateAllPokemonImgUrl);
+        console.log(allPokemonImgUrl);
+        }, 1500 );
+    }
+   
+
 
 /* Events for clicking on game levels. It iterates to check how many cards it needs
 and calls the function getData accordingly. */
 
 $(document).on('click', '#easy', function() {
-    for (var i = 0; i < easy; i++) { 
+    for (var cards = 0; cards < easy; cards++) { 
         getData();
-    } 
+    }
+
+    duplicate();
 })
 
 $(document).on('click', '#medium', function() {
-    for (var i = 0; i < medium; i++) {
+    for (var cards = 0; cards < medium; cards++) {
         getData();
     } 
+
+    duplicate();
+
 })
 
 $(document).on('click', '#hard', function() {
-    for (var i = 0; i < hard; i++) {
+    for (var cards = 0; cards < hard; cards++) {
         getData();
     } 
+
+    duplicate();
+
 })
 
 
