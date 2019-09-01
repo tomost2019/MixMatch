86d84f7081a44143574011700b62f1ef2eac1a3f
Tom Öst <tom.ost@yahoo.com>
2019-09-01 04:58:24 +0200

    Added new functions and the memory game
    Added the actual memory game for the Pokemon Cards.
    
    Functions added:
    
    * playGame = This let's the user to play the game. It checks if the card matches or not. It calls each functions accordingly.
    * victory = This compares the gameCards length with the originalPokemonImgUrl array if it matches.
    
    Added global variables for the arrays matching cards and click events.
    Cleaned up the code and did some indentations.
    Added a lot more comments to each function.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,8 @@ let $loading = $('.loadingDiv').hide();
 let $startPage = $('.start-page').show();
 // Turn information is hidden before the game starts.
 let $countClicks = $('.count-clicks').hide();
-
+// Victory page hidden as standard.
+let $victory = $('.victoryPage').hide();
 
 // Settings for game level. Each integer represents number of cards * 2.
 let easy = 4;
@@ -16,8 +17,13 @@ let originalPokemonImgUrl = [];
 let duplicateAllPokemonImgUrl = [];
 let allPokemonImgUrl = [];
 
-// Arrays for the started game.
-let matchOrNot = [];
+// Arrays for the matching the cards.
+let gameCards = [];
+let matchingCards = [];
+
+// Arrays for clicked events
+let clicked = [];
+let clicked2 = [];
 
 // PokéAPI URL.
 const pokemonDataUrl = 'https://pokeapi.co/api/v2/pokemon/';
@@ -32,6 +38,76 @@ function randomNumber() {
     // Math random function with values from randomnumbers.
     return Math.floor(Math.random() * randomNumberMax) + fromIndex;
 }
+
+// when all cards matches the user wins and the victory page loads.
+function victory() {
+    if (gameCards.length === allPokemonImgUrl.length) {
+        clear();
+        $countClicks.hide(700);
+        $victory.show(700);
+    }
+}
+
+// function for play the game, checks if cards matches or not. Calls the victory function when all the cards are matched. 
+function playGame() {
+    // click function.
+    $(document).on('click', '.card' , function(event) {
+        let $this = $(event.currentTarget)
+        $this.addClass('visible')
+
+        // this pushes click events to arrays to keep track of this and previous this. 
+        clicked.push($this);
+        clicked2.push($this);
+
+        // Pushes the image src into an array to check if there is a match.
+        let matchedCards  = $this.find('img').attr('src')
+        matchingCards.push(matchedCards);
+
+        // if statement to see if the src string matches. 
+        if(matchingCards[0] === matchingCards[1]) {
+
+            // if true then push into a global array that store all the matched cards (image src).
+            gameCards.push(matchingCards[0]);
+            gameCards.push(matchingCards[1]);
+    
+            matchingCards.length = 0; // reset the matchingCards array.
+            
+            // if two card matches then add class matched that removes pointing. 
+            // Keeps track of the actual card from this and previous this array (clicked, clicked2)
+
+            $this.addClass('matched')
+            clicked[0].addClass('matched');
+
+            // removes the data from the arrays that keep tracks of this and previous this. 
+            clicked.length = 0;
+            clicked2.length = 0;
+        
+        } 
+
+        // if the array length is equal to 2 and the strings do not match then reset the array and flip cards to the pokemoncard-back. 
+        if(matchingCards.length == 2 && matchingCards[0] != matchingCards[1]) {
+            
+            matchingCards.length = 0; // resets the matching array. 
+            
+            // flips the cards with a setTimeout. Removes this and previous this data from the arrays  .
+            setTimeout( function(){
+                $this.removeClass('visible');
+                clicked2[0].removeClass('visible');
+                clicked2.length = 0;
+                clicked.length = 0;
+                }, 1000 ); // timer to let the user remember the cards. 
+            
+        }
+        
+        // Run the victory function when all cards are matched. It compares length of gameCards and allPokemonImgUrl.
+        victory(); 
+        
+    })
+    
+}
+
+// Start the game.
+playGame();
 
 // Function for getting data from PokéAPI.
 function getData() {
@@ -74,14 +150,43 @@ function clear() {
     originalPokemonImgUrl.length = 0;
     duplicateAllPokemonImgUrl.length = 0;
     allPokemonImgUrl.length = 0;
+    gameCards.length = 0;
+    clicked.length = 0;
+    clicked2.length = 0;
     $('#output').empty();
 }
+
+// Shuffle code from css-tricks.com.
+function Shuffle(cards) {
+	for(var j, x, i = cards.length; i; j = parseInt(Math.random() * i), x = cards[--i], cards[i] = cards[j], cards[j] = x);
+	return cards;
+}; 
+
+// function iterates through allPokemonImgUrl array and appends the output.
+function output() {
+    allPokemonImgUrl.forEach(function (pokemonUrl) {
+        $('#output').append
+        
+        (`
+
+            <div class="col-4 col-sm-3 card-container pb-2">
+            <div class="card mx-auto">
+            <div class="pokemoncard-back transformation"></div>
+            <div class="pokemoncard-front transformation"><img class="pt-3" src="${[pokemonUrl]}"></div>
+            </div>
+            </div>
+
+        `);
+        
+    }
+)}
 
 /* This function copies the array so that we always have two of the same cards. 
 Then concat into a new array and shuffles it. After that it outputs the result */
 function startGame(){
     
     setTimeout( function(){
+        $victory.hide(500);
         $countClicks.hide(500); // Hides the turns count.
         $startPage.hide(500); // Hides the startPage when game starts. 
         $loading.show(1000); // Show loading pikachu.
@@ -106,79 +211,9 @@ function startGame(){
         }, 4500 );
     }
 
-// Shuffle code from css-tricks.com.
-function Shuffle(cards) {
-	for(var j, x, i = cards.length; i; j = parseInt(Math.random() * i), x = cards[--i], cards[i] = cards[j], cards[j] = x);
-	return cards;
-}; 
 
-// function iterates through allPokemonImgUrl array and appends the output.
-function output() {
-    allPokemonImgUrl.forEach(function (pokemonUrl) {
-        $('#output').append
-        
-        (`
-
-            <div class="col-4 col-sm-3 card-container pb-2">
-            <div class="card mx-auto">
-            <div class="pokemoncard-back transformation"></div>
-            <div class="pokemoncard-front transformation"><img class="pt-3 card-value" src="${[pokemonUrl]}"></div>
-            </div>
-            </div>
-
-        `);
-        
-    }
-)}
-
-// When clicking each card it adds the class visible so that the front card is visible. 
-
-/*
-function clickCard() {
-    $(document).ready(function() {
-        $('.card').on('click', e => {
-            let $this = $(e.currentTarget)
-            $this.addClass('visible')
-            let cardValue = $this.attr('src');
-            matchOrNot.push(cardValue);
-    }
-)}
-
-)}
-*/
-
-
-function clickCard() {
-    $(document).ready(function() {
-
-        $(document).on('click', '.card' , e => {
-            let $this = $(e.currentTarget)
-            $this.addClass('visible')
-            let cardValue = $this.attr('src');
-            matchOrNot.push(cardValue);
-    
-            console.log(matchOrNot)
-
-    })
-
-    })
-}
-
-
-
-/*
-function clickCard() {
-    $(document).on('click', '.card', function() {
-        $(this).addClass('visible');
-
-        var cardValue = $(this).attr('src');
-        matchOrNot.push(cardValue);
-
-    })
-}*/
-   
 /* Events for clicking on game levels. It iterates to check how many cards it needs
-and calls the function getData accordingly. */
+and calls functions accordingly. */
 
 $(document).on('click', '#easy', function() {
     for (var cards = 0; cards < easy; cards++) { 
@@ -187,8 +222,6 @@ $(document).on('click', '#easy', function() {
     disableButtons();   
     clear();
     startGame();
-    clickCard();
- 
 })
    
 $(document).on('click', '#medium', function() {
@@ -198,7 +231,7 @@ $(document).on('click', '#medium', function() {
     disableButtons();   
     clear();
     startGame();
-    clickCard();
+
 })
 
 $(document).on('click', '#hard', function() {
@@ -208,7 +241,6 @@ $(document).on('click', '#hard', function() {
     disableButtons();   
     clear();
     startGame();
-    clickCard();
 })
 
 
